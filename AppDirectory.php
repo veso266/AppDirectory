560f19d06dbf72e399e8dced70766ebf23b9c10d
veso266 <veso266@gmail.com>
2018-10-25 19:57:48 +0200

    aaa

--- AppDirectory.php
+++ AppDirectory.php
@@ -1,25 +1,16 @@
 <?php
-require_once ('zend/vendor/autoload.php');
-require_once ('classes/AppDirectory.php');
+require_once ('classes2/App.php');
 
-$serverUrl = "http://appdirectory.messenger.msn.com/AppDirectory/AppDirectory.php";
-$options = [
-    'uri' => $serverUrl,
-];
-$server = new Zend\Soap\Server(null, $options);
+$options= array('uri'=>'http://appdirectory.messenger.msn.com/AppDirectory/AppDirectory.php', 'location' => 'http://appdirectory.messenger.msn.com/AppDirectory/AppDirectory.php');
 
-if (isset($_GET['wsdl'])) {
-    $soapAutoDiscover = new \Zend\Soap\AutoDiscover(new \Zend\Soap\Wsdl\ComplexTypeStrategy\ArrayOfTypeSequence());
-    $soapAutoDiscover->setBindingStyle(array('style' => 'document'));
-    $soapAutoDiscover->setOperationBodyStyle(array('use' => 'literal'));
-    $soapAutoDiscover->setClass('AppDirectory');
-    $soapAutoDiscover->setUri($serverUrl);
 
-    header("Content-Type: text/xml");
-    echo $soapAutoDiscover->generate()->toXml();
-} else {
-    $soap = new \Zend\Soap\Server($serverUrl . '?wsdl');
-    $soap->setObject(new \Zend\Soap\Server\DocumentLiteralWrapper(new AppDirectory()));
-    $soap->handle();
-}
+$server = new SoapServer(NULL, $options);
+$server->setClass('AppDirectory');
+$server->setObject(new AppDirectory());
+$server->handle();
+
+
+//print "<pre>";
+//print_r($server->getFunctions());
+//print "</pre>";
 ?>
